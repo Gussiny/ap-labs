df68a9fc8f0d175521622dcc5d7b511da5440618
Gustavo A Flores <a01635151@itesm.mx>
2020-08-28 13:31:27 -0500

    Update code and check test
    Signed-off-by: Gustavo A Flores <a01635151@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+#define IN 	1
+#define OUT 0
 
 int main(){
     // Place your magic here
+    int newLine, state, newWord, newChar;
+    char c;
+    char words[256]="";
+    state = OUT;
+    newLine = newWord = newChar = 0;
+    while((c = getchar()) != EOF){
+    	
+    	if(c == '\n'){
+    		newLine++;
+    		for(int x = newChar; x >= 0; x--){
+    			printf("%c", words[x]);
+    		}
+    		printf("\n");
+    		memset(words, 0, sizeof words);
+    	} else{
+    		words[newChar] = c;
+    		newChar++;
+    	}
+
+    }
     return 0;
 }
