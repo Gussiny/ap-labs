f11be981972696cf0a18506de2698883aa4fa9aa
Gustavo A Flores <a01635151@itesm.mx>
2020-09-07 13:51:10 -0500

    Solve str-lib lab
    Signed-off-by: Gustavo A Flores <a01635151@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char *argv[])
+{
+	/* code */
+	if(argc < 2){
+		printf("Invalid arguments\n");
+	} else if(mystrfind(argv[1],"-add") >= 0){
+		if(argc < 4){
+			printf("Invalid arguments for add\n");
+		} else{
+			int lenght = mystrlen(argv[2]);
+			char *newString = mystradd(argv[2],argv[3]);
+			int newLength = mystrlen(newString);
+
+			printf("Initial Lenght\t: %d \n", lenght);
+			printf("New String\t: %s \n", newString);
+			printf("New Lenght\t: %d \n", newLength);
+
+		}
+	} else if(mystrfind(argv[1],"-find") >= 0 ){
+		if(argc < 4){
+			printf("Invalid arguments for find\n");
+		} else{
+			int position = mystrfind(argv[2], argv[3]);
+			if(position >= 0){
+				printf("[\'%s\'] string was found at [%d] position\n", argv[3], position);
+			} else{
+				printf("The string was not found\n");
+			}
+
+		}
+	}
+	return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,58 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+	int lenght = 0;
+	while(str[lenght] != '\0'){
+		lenght++;
+	}
+    return lenght;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+	int lenOrigin = mystrlen(origin);
+	int lenAddition = mystrlen(addition);
+	char *str = malloc(lenOrigin + lenAddition);
+	int i = 0;
+
+	while(origin[i] != '\0'){
+		str[i] = origin[i];
+		i++;
+	}
+	
+	i = 0;
+	while(addition[i] != '\0'){
+		str[i+lenOrigin] = addition[i];
+		i++;
+	}
+
+	str[lenOrigin + lenAddition] = '\0';
+    return str;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+	int lenOrigin = mystrlen(origin);
+	int lenSubst = mystrlen(substr);
+	int i = 0;
+	int position = -1;
+	while(i < lenOrigin){
+
+		if(origin[i] == substr[0]){
+			int j = 1;
+			position = i;
+			while(j < lenSubst){
+				if(origin[i+j] != substr[j]){
+					position = -1;
+					break;
+				}
+				j++;
+			}
+			if(j == lenSubst){
+				return position;
+			}
+		}
+		i++;
+	}
+
+    return position;
 }
