97920b47eb5515ced40ba77554b801bc0db90017
Gustavo Alejandro Flores Cortes <a01635151@itesm.mx>
2020-11-10 13:34:37 -0600

    Solve pipeline
    Signed-off-by: Gustavo Alejandro Flores Cortes <a01635151@itesm.mx>

--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var waitChan = make(chan struct{})
 
+func pipeline(in chan struct{}, out chan struct{}, current int, total int) {
+	if current <= total {
+		go pipeline(out, in, current+1, total)
+		out <- <-in
+	} else {
+		waitChan <- struct{}{}
+	}
+}
+
+func main() {
+	var maxStages = 200000
+	var startTime = time.Now()
+
+	in := make(chan struct{})
+	out := make(chan struct{})
+
+	go pipeline(in, out, 0, maxStages)
+	<-waitChan
+
+	transitTime := time.Since(startTime)
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
 	fmt.Println("Time to transit trough the pipeline : ", transitTime)
 }
